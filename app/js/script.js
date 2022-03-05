70c67a11073a550fda7365246d80c2d9a2766450
Abdelghafour <ghafourragg@gmail.com>
2022-03-05 10:56:08 +0100

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,8 @@
 const hamButton = document.querySelector("#ham");
 const outlaySec = document.querySelector(".outlay");
 
-const openImg =
-  "http://127.0.0.1:5500/loopstudios-landing-page/images/icon-hamburger.svg";
-const closeImg =
-  "http://127.0.0.1:5500/loopstudios-landing-page/images/icon-close.svg";
+const openImg = "./images/icon-hamburger.svg";
+const closeImg = "./images/icon-close.svg";
 hamButton.addEventListener("click", () => {
   hamButton.src === openImg
     ? (hamButton.src = closeImg)
