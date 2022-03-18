a6c8a0b2a2170465b3d04158a85c83dfecc7ad35
Abdelghafour <ghafourragg@gmail.com>
2022-03-18 20:17:17 +0100

    fixing menu ham image

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,15 @@
 const hamButton = document.querySelector("#ham");
 const outlaySec = document.querySelector(".outlay");
 
-const openImg =
-  "http://127.0.0.1:5500/loopstudios-landing-page/images/icon-hamburger.svg";
-const closeImg =
-  "http://127.0.0.1:5500/loopstudios-landing-page/images/icon-close.svg";
+const openImg = "./images/icon-hamburger.svg";
+const closeImg = "./images/icon-close.svg";
+// const openImg =
+//   "http://127.0.0.1:5500/loopstudios-landing-page/images/icon-hamburger.svg";
+// const closeImg =
+//   "http://127.0.0.1:5500/loopstudios-landing-page/images/icon-close.svg";
 hamButton.addEventListener("click", () => {
-  hamButton.src === openImg
+  // hamButton.src === openImg
+  hamButton.src.includes(openImg.slice(1))
     ? (hamButton.src = closeImg)
     : (hamButton.src = openImg);
   outlaySec.classList.toggle("fade-in");
